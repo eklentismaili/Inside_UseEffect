fabdbf68f5fa51fe15d5f22f3350d401aa0322fc
Eklent Ismaili <ismailieklent3@gmail.com>
2022-09-18 20:16:57 +0200

    side effect on rendering process

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import React from "react";
 function App() {
   const [todos, setTodos] = React.useState([]);
 
+  (function fetchData() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setTodos(response))
+      .then((json) => console.log(json));
+  })();
+
+  // Running a side effect on the render process such as a network req !!!
+
   return <div className="App">hi</div>;
 }
 
