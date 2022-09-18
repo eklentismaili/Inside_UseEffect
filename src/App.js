73d193527bbd02a7448379dc9d95bf3446b7d839
Eklent Ismaili <ismailieklent3@gmail.com>
2022-09-18 21:57:56 +0200

    Side effect dom manipulation on render

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import axios from "axios";
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 
-function App() {
-  const [todos, setTodos] = React.useState([]);
+function VideoPlayer({ src, isPlaying }) {
+  const ref = useRef(null);
 
-  (function fetchData() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => setTodos(response))
-      .then((json) => console.log(json));
-  })();
+  if (isPlaying) {
+    ref.current.play(); // Calling these while rendering isn't allowed.
+  } else {
+    ref.current.pause(); // Also, this crashes.
+  }
 
-  // Running a side effect on the render process such as a network req !!!
-
-  return <div className="App">hi</div>;
+  return <video ref={ref} src={src} loop playsInline />;
 }
 
-export default App;
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <>
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <VideoPlayer
+        isPlaying={isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
+    </>
+  );
+}
+
+/* The reason this code isn’t correct is that it tries to do something with the DOM node during rendering. 
+In React, rendering should be a pure calculation of JSX and should not contain side effects like modifying the DOM.
+
+Moreover, when VideoPlayer is called for the first time, its DOM does not exist yet! 
+There isn’t a DOM node yet to call play() or pause() on, because React doesn’t know what DOM to create until after you return the JSX. */
